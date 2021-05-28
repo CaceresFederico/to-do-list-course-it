52b9e5b09580adcd6527f3cbdb7d3ad3832f689c
Federico Cáceres <caceresfederico@hotmail.com>
2021-05-28 17:18:52 -0300

    v1.2 css and js improved

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-let list = document.getElementById('list');
-const titulo = document.getElementById('titulo');
 const h1 = createElements('h1', "to do list", 'titulo');
 
-function addItem() { //Generando el elemento con createElement
+//Generando el elemento con createElement
+function addItem() { 
     let input = document.getElementById('inputValue').value;
-    if (input.length != 0) createElements('li', input, 'list', input);
+    input.length != 0 ? createElements('li', input, 'list', input): alert("input vacío")
+    document.getElementById('inputValue').value = ''
 };
 
-function enterEvent(e) { //Captura el Enter
-    if (e.code == 'Enter' ) addItem();
-}
+//Captura el Enter pasando el evento como parámetro
+function enterEvent(e) { if (e.code == 'Enter' ) addItem();}
 
-function erase(e){ //Remueve matcheando el e.target.textContext con el id de cada li que incluimos en la funcion createELements
+//Remueve matcheando el e.target.textContext con el id de cada li que incluimos en la funcion createELements();
+function erase(e){ 
     let toErase = document.getElementById(e.target.textContent);
-    if (toErase)toErase.remove();
+    if (toErase) toErase.remove();
 }
 
-function createElements(type, content, parentId, id) { //Función reutilizable utilizada para generar los elementos y renderizarlos
+//Función reutilizable utilizada para generar los elementos y renderizarlos
+function createElements(type, content, parentId, id) { 
     const parent = document.getElementById(parentId);
     const task = document.createElement(type);
     const taskName = document.createTextNode(content);
